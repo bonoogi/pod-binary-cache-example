89be064e045d5f30c5061c5996f86c18b1617fe2
Koo Bonwook <bnoo1333@gmail.com>
2021-08-08 23:51:07 +0900

    Proj init

---
+++ App/PodCacheApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PodCacheApp
+//
+//  Created by 구본욱 on 2021/08/08.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ App/PodCacheApp/PodCacheAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PodCacheAppApp.swift
+//  PodCacheApp
+//
+//  Created by 구본욱 on 2021/08/08.
+//
+
+import SwiftUI
+
+@main
+struct PodCacheAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
